69a9ab0b4dcb53d604e234ac2ceec19027d6a2ab
Alexander Kluchkov <136575524+AlexKluchkov@users.noreply.github.com>
2025-07-25 08:34:51 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,7 +22,8 @@ string VehicleCardRegistrationDate = "";
 string VehicleCardVhicleColor = "";
 string VehicleCardVehicleMake = "";
 
-string Insurance_Policy_Template_Path = Path.Combine(AppContext.BaseDirectory, "Auto_Insurance_Policy_Template.txt");
+var basePath = AppContext.BaseDirectory;
+var Insurance_Policy_Template_Path = Path.Combine(basePath, "Auto_Insurance_Policy_Template.txt");
 string Insurance_Policy_Path = Path.Combine(AppContext.BaseDirectory, "Auto_Insurance_Policy.txt");
 
 var me = await bot.GetMe();
